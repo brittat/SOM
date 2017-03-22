ad30349c3bc9c525ec7454666ee7c23d2715eefc
Britta Thörnblom <brittat@student.chalmers.se>
2017-03-22 16:15:32 -0400

    Added decay method needed for neighborhood size and learning rate during training.

--- kohonen/src/main/java/KohonenMap.java
+++ kohonen/src/main/java/KohonenMap.java
@@ -10,17 +10,24 @@ public class KohonenMap {
   private double learningParam;
   // Parameter for neighborhood size function
   private double neighborParam;
+  private double t;
 
   public KohonenMap(int verticalNodes, int horizontalNodes, double learningParam, double neighborParam) {
     this.verticalNodes = verticalNodes;
     this.horizontalNodes = horizontalNodes;
     this.learningParam = learningParam;
     this.neighborParam = neighborParam;
+    this.t = 0;
   }
 
   public void train() {
     // Loop over all input vectors
     // Competitive learning: find the winner; update winner according to learning rate function
     // Also update neighbors according to their distance from the winner
+  }
+
+  public double decay(double factor, double denominator) {
+    double argument = - this.t/denominator;
+    return factor*Math.exp(argument);
   }
 }
