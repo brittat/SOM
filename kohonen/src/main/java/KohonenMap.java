911d366d1f84d9130bfab414af01fd01a027bd28
Britta Thörnblom <brittat@student.chalmers.se>
2017-03-23 15:33:16 -0400

    Added magnitude method for evaluating vector cosine similarity

--- kohonen/src/main/java/KohonenMap.java
+++ kohonen/src/main/java/KohonenMap.java
@@ -64,4 +64,12 @@ public class KohonenMap {
     }
     return dotProduct;
   }
+
+  public double getMagnitude(double[] vector) {
+    double sum = 0;
+    for (int i = 0; i < vector.length; i ++) {
+      sum += Math.pow(vector[i], 2);
+    }
+    return Math.sqrt(sum);
+  }
 }
